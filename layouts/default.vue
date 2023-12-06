453cd8efab2ab71bdba4d253ee99e8e68bf53fc8
Andre Ribeiro <andrefigrib@gmail.com>
2023-12-06 14:50:47 -0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header></header>
+        <div class="content">
+            <slot />
+        </div>
+        <footer></footer>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+</style>
